3ff1492c34b9543c67d52381de396aca747a320a
Bayu Maulana <bayufransdo@gmail.com>
2024-04-14 21:51:30 +0700

    Change image using webp instead

--- register.php
+++ register.php
@@ -7,11 +7,11 @@
   <title>Register</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="./src/styles/style.css">
   <link rel="shortcut icon" href="./src/images/logo.png" type="image/x-icon">
 </head>
 
-<body class="bg-image" style="background-image: url('./src/images/foto.png')">
+<body class="bg-image" style="background-image: url('./src/images/foto.webp')">
   <div class="container-fluid">
     <div class="row justify-content-center align-item-center d-flex">
       <div class="col position-absolute top-50 start-50 translate-middle">
